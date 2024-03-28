a61f075ec96e9b81921bf6a76fa56898d0aa3721
raneem <raneem.hamzah@gmail.com>
2024-03-28 23:19:40 +0300

    edit5

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/project6-react",
+
   plugins: [react()],
+  base: "/project6-react"
 
 })
